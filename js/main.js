e3e873ad8ce7a7bb521abe7949bffa1e891cafb9
Ugnius <ugniues@yahoo.com>
2020-04-05 21:15:38 +0300

    Pridėta funkcija veidu detektavimui ir atvaizdavimui

--- js/main.js
+++ js/main.js
@@ -3,6 +3,25 @@ const video = window.video = document.querySelector('video');
 const canvas = window.canvas = document.querySelector('canvas');
 canvas.width = 480;
 canvas.height = 360;
+
+function drawFrame() {
+  const context = canvas.getContext('2d');
+  context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+  const faces = ccv.detect_objects({ // detect faces
+    canvas: ccv.pre(canvas),
+    cascade: cascade,
+    interval: 5,
+    min_neighbors: 1
+  });
+
+  faces.forEach(face => { // draw rectangles on detected faces
+    context.lineWidth = "10";
+    context.strokeStyle = "blue";
+    context.strokeRect(face.x, face.y, face.width, face.height);
+  });
+  
+}
 const constraints = {
   audio: false,
   video: true
